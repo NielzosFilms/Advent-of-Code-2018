b91fac5d013995c1f0d54185b7b912eaeabc3f5d
niels.hazelaar <niels.hazelaar@feka.nl>
2020-10-29 16:58:06 +0100

    day 1

---
+++ src/day_1/ChronalCalibration.java
@@ -0,0 +1,56 @@
+package day_1;
+
+import helpers.TextFile;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.ArrayList;
+
+public class ChronalCalibration {
+    private TextFile textFile;
+
+    private static final int START_FREQ = 0;
+
+    private int freq;
+    private ArrayList<Integer> frequencies = new ArrayList<Integer>();
+
+    public ChronalCalibration() throws ScriptException {
+
+        this.textFile = new TextFile("inputs/day_1/input.txt");
+        this.freq = START_FREQ;
+        ArrayList<String> changes = this.textFile.getLines();
+        this.frequencies.add(START_FREQ);
+
+        boolean running = true;
+        while(running) {
+            for (String change : changes) {
+                int change_int = Integer.parseInt(change);
+                this.freq += change_int;
+
+                if (frequencyExists(this.freq)) {
+                    running = false;
+                    break;
+                } else {
+                    this.frequencies.add(this.freq);
+                }
+            }
+        }
+        System.out.println(this.freq);
+    }
+
+    private boolean frequencyExists(int freq) {
+        for(int found_freq : this.frequencies) {
+            if(found_freq == freq) return true;
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        try {
+            new ChronalCalibration();
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+    }
+}
