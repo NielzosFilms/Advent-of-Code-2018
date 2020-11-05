24cfe8bc247897bcaca50cbaaa61aad6105c362f
niels.hazelaar <niels.hazelaar@feka.nl>
2020-11-05 14:29:07 +0100

    day 3 complete

--- src/day_3/Claim.java
+++ src/day_3/Claim.java
@@ -1,9 +1,12 @@
 package day_3;
 
 import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Claim {
 	private String claim;
+	private boolean overlap;
 
 	private int ID;
 	private int x, y, w, h;
@@ -11,6 +14,7 @@ public class Claim {
 	public Claim(String claim) {
 		this.claim = claim;
 		decodeClaim(this.claim);
+		this.overlap = false;
 	}
 
 	private void decodeClaim(String claim) {
@@ -21,14 +25,26 @@ public class Claim {
 		this.h = Integer.parseInt(claim.substring(claim.indexOf('x')+1));
 	}
 
+	public ArrayList<Point> getAllCoords() {
+		ArrayList<Point> coords = new ArrayList<Point>();
+		for(int yy = 0;yy < this.h; yy++) {
+			for(int xx = 0;xx < this.w; xx++) {
+				coords.add(new Point(this.x + xx, this.y + yy));
+			}
+		}
+		return coords;
+	}
+
+	public void setOverlap(boolean overlap) {
+		this.overlap = overlap;
+	}
+
+	public boolean getOverlap() {
+		return this.overlap;
+	}
+
 	public int getID() {
 		return this.ID;
-	}
-	public Rectangle getBounds() {
-		return new Rectangle(this.x, this.y, this.w, this.h);
-	}
-	public String getClaim() {
-		return this.claim;
 	}
 
 }
--- src/day_3/Day3.java
+++ src/day_3/Day3.java
@@ -1,89 +1,51 @@
 package day_3;
 
 import helpers.TextFile;
-import org.w3c.dom.css.Rect;
 
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 
 public class Day3 {
-	private ArrayList<String> lines = new ArrayList<String>();
-	private ArrayList<Claim> claims = new ArrayList<Claim>();
-	private ArrayList<Overlap> overlaps = new ArrayList<Overlap>();
+	private final ArrayList<Claim> claims = new ArrayList<Claim>();
+	private ArrayList<Point> claimed_coords = new ArrayList<Point>();
+	private ArrayList<Point> counted_coords = new ArrayList<Point>();
+
+	private int multiple_claim_count = 0;
 
 	public Day3() {
 		TextFile textFile = new TextFile("inputs/day_3/input.txt");
-		this.lines = textFile.getLines();
-
-		decodeLines();
-		int tmp = calculateOverlap();
-		int output = tmp - getDiff();
-		System.out.println(output);
-	}
-
-	private int calculateOverlap() {
-		int res = 0;
-		for(Claim claim_1 : claims) {
-			System.out.println("calculateOverlap ID: " + claim_1.getID());
-			for(Claim claim_2 : claims) {
-				if(claim_1 == claim_2) continue;
-				if(claim_1.getBounds().intersects(claim_2.getBounds())) {
-					if(!containsOverlap(this.overlaps, claim_1.getID(), claim_2.getID())) {
-						Rectangle2D overlap = claim_1.getBounds().createIntersection(claim_2.getBounds());
-						res += overlap.getWidth() * overlap.getHeight();
-						addOverlap(claim_1.getID(), claim_2.getID(), overlap);
-					}
+		for(String line : textFile.getLines()) {
+			this.claims.add(new Claim(line));
+		}
+		for(Claim claim : claims) {
+			for(Point coord : claim.getAllCoords()) {
+				addClaimedCoord(coord, claim);
+			}
+		}
+		for(Claim claim : claims) {
+			boolean overlap = false;
+			for(Point coord : claim.getAllCoords()) {
+				int count = Collections.frequency(this.claimed_coords, coord);
+				if(count > 1) {
+					overlap = true;
+					break;
 				}
 			}
+			if(!overlap) System.out.println("No overlap ID: " + claim.getID());
 		}
-		return res;
+		System.out.println(multiple_claim_count);
 	}
 
-	private void addOverlap(int ID_1, int ID_2, Rectangle2D overlap) {
-		overlaps.add(new Overlap(ID_1, ID_2, overlap));
-	}
-
-	private int getDiff() {
-		HashMap<Overlap, ArrayList<Overlap>> checkedOverlaps = new HashMap<>();
-		int sub = 0;
-		for(Overlap tmp_1 : overlaps) {
-			System.out.println("getDiff ID: " + tmp_1.ID_1);
-			ArrayList<Overlap> checked = new ArrayList<Overlap>();
-			checkedOverlaps.put(tmp_1, checked);
-			for(Overlap tmp_2 : overlaps) {
-				if(tmp_1 != tmp_2) {
-					if(checkedOverlaps.containsKey(tmp_2)) {
-						if (!checkedOverlaps.get(tmp_2).contains(tmp_1)) {
-							if (tmp_1.overlap.getBounds().intersects(tmp_2.overlap.getBounds())) {
-								Rectangle2D overlap = tmp_1.overlap.getBounds().createIntersection(tmp_2.overlap.getBounds());
-								sub += overlap.getWidth() * overlap.getHeight();
-								checked.add(tmp_2);
-							}
-						}
-					}
-				}
+	private void addClaimedCoord(Point coord, Claim claim) {
+		/*if(this.claimed_coords.contains(coord)) {
+			if(!this.counted_coords.contains(coord)) {
+				this.multiple_claim_count++;
+				this.counted_coords.add(coord);
 			}
-			checkedOverlaps.put(tmp_1, checked);
-		}
-		return sub;
-	}
-
-	private boolean containsOverlap(ArrayList<Overlap> overlaps, int ID_1, int ID_2) {
-		for(Overlap overlap : overlaps) {
-			if(overlap.checkIDs(ID_1, ID_2)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private void decodeLines() {
-		for(String line : lines) {
-			claims.add(new Claim(line));
-		}
+			return;
+		}*/
+		this.claimed_coords.add(coord);
 	}
 
 	public static void main(String args[]) {
